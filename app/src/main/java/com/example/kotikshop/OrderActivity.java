8703059441d924d74f0462616e8421a197cd3d79
Arturirurich <Arturkawin@gmail.com>
2023-04-11 09:03:07 +0300

    KotikShop 11.04 Active email

--- app/src/main/java/com/example/kotikshop/OrderActivity.java
+++ app/src/main/java/com/example/kotikshop/OrderActivity.java
@@ -3,12 +3,15 @@ package com.example.kotikshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
-    String[] addresses = {" allakhverdovyuriy@gmail.com "};
+    String[] addresses = {" vika.viktor2281.volkova@mail.ru "};
     String subject = "";
+    String emailText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +26,21 @@ public class OrderActivity extends AppCompatActivity {
         double price = recievedOrderIntent.getDoubleExtra("price",0);
         double orderPrice = recievedOrderIntent.getDoubleExtra("orderPrice",0);
 
+        emailText = "Customer name: "+userName + "\n" +"Goods name: "+ goodsName + "\n" +"Quantity: "+ quantity + "\n" + "Price: "+price + "\n" + "Order price: "+orderPrice;
         TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer name: "+userName + "\n" +"Goods name: "+ goodsName + "\n" +"Quantity: "+ quantity + "\n" + "Price: "+price + "\n" + "Order price: "+orderPrice);
+        orderTextView.setText(emailText);
 
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
     }
 }
